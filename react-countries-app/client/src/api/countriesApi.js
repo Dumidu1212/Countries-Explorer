a5cbf8adfd1a429ac6ed4d45a8be32dd8afd2bca
dumidu.rajapaksha <91795441+Dumidu1212@users.noreply.github.com>
2025-09-17 00:06:34 +0530

    Resolved API error

--- react-countries-app/client/src/api/countriesApi.js
+++ react-countries-app/client/src/api/countriesApi.js
@@ -2,8 +2,10 @@
 import axios from 'axios';
 
 const BASE = 'https://restcountries.com/v3.1';
+const FIELDS = "name,cca2,cca3,capital,region,subregion,population,languages,flags";
 
-export const fetchAllCountries       = () => axios.get(`${BASE}/all`).then(r => r.data);
+export const fetchAllCountries = () =>
+  axios.get(`${BASE}/all?fields=${FIELDS}`).then((r) => r.data);
 export const fetchCountriesByName    = (n) => axios.get(`${BASE}/name/${n}`).then(r => r.data);
 export const fetchCountriesByRegion  = (r) => axios.get(`${BASE}/region/${r}`).then(r => r.data);
 export const fetchCountriesByLanguage= (l) => axios.get(`${BASE}/lang/${l}`).then(r => r.data);
